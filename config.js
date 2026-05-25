0f5786c9362b9f81baa374591ea349cd8aecebb3
chessyokjvl <qualityjvl@gmail.com>
2026-05-25 11:24:24 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    // นำ URL ที่ได้จากการ Deploy Google Apps Script มาวางแทนข้อความด้านล่างนี้
+    API_URL: "https://script.google.com/macros/s/AKfycbwzW79VBoyp3HCKkoPqwhvpO1VoekjAdLMLUvQGlK1txHxre0gL5nFiyYa9_ENTMsFx/exec",
+    
+    MODULES: {
+        PATIENTS: "Patients",
+        STAFF: "Staff",
+        AAR_RCA: "AAR_RCA"
+    }
+};
